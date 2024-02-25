012ff7fecadfee77460d83f325f2990a6afe6c0d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-25 11:00:53 +0100

    Implement ArticleResponse

--- src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.kwi.chrisblog.dtos.ArticleRequest;
+import pl.kwi.chrisblog.dtos.ArticleResponse;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class ArticleController {
 
     @PostMapping("api/v1/article")
-    public String findArticlesByHomeCategory(@RequestBody ArticleRequest request) {
+    public ArticleResponse findArticlesByHomeCategory(@RequestBody ArticleRequest request) {
 
         System.out.println("request: " + request.toString());
 
-        return "Ok";
+        return new ArticleResponse("Ok");
 
     }
     
---
+++ src/main/java/pl/kwi/chrisblog/dtos/ArticleResponse.java
@@ -0,0 +1,5 @@
+package pl.kwi.chrisblog.dtos;
+
+public record ArticleResponse(String message) {
+    
+}
