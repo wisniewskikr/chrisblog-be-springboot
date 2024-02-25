81e160562dd690cf995a6949551b2507da7a795d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-25 10:46:18 +0100

    Update ArticleController

--- src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
@@ -1,10 +1,6 @@
 package pl.kwi.chrisblog.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +12,12 @@ import pl.kwi.chrisblog.dtos.ArticleRequest;
 public class ArticleController {
 
     @PostMapping("api/v1/article")
-    public List<String> findArticlesByHomeCategory(@RequestBody ArticleRequest request) {
+    public String findArticlesByHomeCategory(@RequestBody ArticleRequest request) {
 
         System.out.println("request: " + request.toString());
 
-        List<String> result = new ArrayList<>();
-        result.add("Test");
-        return result;
+        return "Ok";
+
     }
     
 }
