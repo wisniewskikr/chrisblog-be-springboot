538875281212d31c46f46ae0ee144503548ad2f0
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-25 09:07:42 +0100

    Add CrossOrigin

--- src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/ArticleController.java
@@ -3,6 +3,7 @@ package pl.kwi.chrisblog.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.kwi.chrisblog.dtos.ArticleRequest;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class ArticleController {
 
     @GetMapping("api/v1/article")
