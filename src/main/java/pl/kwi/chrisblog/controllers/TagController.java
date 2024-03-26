31b2da3d0d226c4713681dada7d8ff5fd746c794
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-03-26 17:25:29 +0100

    Update from Post to Get for Tag

--- src/main/java/pl/kwi/chrisblog/controllers/TagController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/TagController.java
@@ -2,8 +2,8 @@ package pl.kwi.chrisblog.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.kwi.chrisblog.dtos.TagRequest;
@@ -24,9 +24,15 @@ public class TagController {
     }
 
 
-    @PostMapping("api/v1/tag")
-    public TagResponse findTags(@RequestBody TagRequest tagRequest) {
-        return tagService.findTags(tagRequest);
+    @GetMapping("api/v1/tag")
+    public TagResponse findTags(
+        @RequestParam("categoryId") Long categoryId,
+        @RequestParam(value = "tagId", required = false) Long tagId,
+        @RequestParam("page") int page,
+        @RequestParam("sorting") String sorting,
+        @RequestParam(value = "searchText", required = false) String searchText 
+    ) {
+        return tagService.findTags(new TagRequest(categoryId, tagId, page, sorting, searchText));
     }
     
     
