d3aa659218320a7ddcbe207659ddbb0b859ea7d0
Chris <wisniewskikr@gmail.com>
2025-11-13 19:00:26 +0100

    Add article about Wroclaw

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3457,3 +3457,15 @@ insert into ARTICLE (id, page_description, title, description, date, author, cat
 'LINK_ALBUM',
 'https://photos.app.goo.gl/zuaFonujQU25Lt6g6');
 insert into ARTICLE_TAG (tag_id, article_id) values (67,277);
+
+insert into ARTICLE (id, page_description, title, description, date, author, category_id, template, url) values (
+278,
+'The article presents a relation from a trip to Wroclaw in Poland.',
+'Wroclaw, Poland, 07.2024', 
+'The article presents a relation from a trip to Wroclaw in Poland in July 2024.', 
+'2025-11-13', 
+'Chris', 
+4,
+'LINK_ALBUM',
+'https://photos.app.goo.gl/oDuMTaTHp23UPpjE7');
+insert into ARTICLE_TAG (tag_id, article_id) values (32,278);
