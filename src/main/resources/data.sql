7e2aa073b06dd747f29dd5291e52b1a452b3e64c
Chris <wisniewskikr@gmail.com>
2026-02-23 15:54:11 +0100

    Add article about Opole

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3662,3 +3662,15 @@ insert into ARTICLE (id, page_description, title, description, date, author, cat
 'LINK_ALBUM',
 'https://photos.app.goo.gl/YrqxqVHJVinrpJFY8');
 insert into ARTICLE_TAG (tag_id, article_id) values (32,294);
+
+insert into ARTICLE (id, page_description, title, description, date, author, category_id, template, url) values (
+295,
+'The article presents a relation from a trip to Opole in Poland.',
+'Opole, Poland, 08.2024', 
+'The article presents a relation from a trip to Opole in Poland in August 2024.', 
+'2026-02-23', 
+'Chris', 
+4,
+'LINK_ALBUM',
+'https://photos.app.goo.gl/ZQbK6DTRG1tDr8tf7');
+insert into ARTICLE_TAG (tag_id, article_id) values (32,295);
