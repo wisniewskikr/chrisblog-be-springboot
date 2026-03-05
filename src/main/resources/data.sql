4dd8707eb5bf1a7bb75a8879ce69e77cb5277e26
Chris <wisniewskikr@gmail.com>
2026-03-05 11:13:01 +0100

    Add article about Zakopane

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3854,3 +3854,15 @@ insert into ARTICLE (id, page_description, title, description, date, author, cat
 'LINK_ALBUM',
 'https://photos.app.goo.gl/rPkjkcyN24u5K8v98');
 insert into ARTICLE_TAG (tag_id, article_id) values (32,310);
+
+insert into ARTICLE (id, page_description, title, description, date, author, category_id, template, url) values (
+311,
+'The article presents a relation from a trip to Zakopane in Poland.',
+'Zakopane, Poland, 09.2025', 
+'The article presents a relation from a trip to Zakopane in Poland in September 2025.', 
+'2026-03-05', 
+'Chris', 
+4,
+'LINK_ALBUM',
+'https://photos.app.goo.gl/LYAGrNwZPYxreKLF9');
+insert into ARTICLE_TAG (tag_id, article_id) values (32,311);
